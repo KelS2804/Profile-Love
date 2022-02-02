662a72bfbd4f9fe6ac2413ee86c34eef28f7ce06
KelS2804 <ndson2804@gmail.com>
2022-02-02 21:38:13 +0700

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+
+$('button').on('click', function(){
+    $('.modal').show()
+    $('.modal-content').animate({opacity: 1, top: 300})
+})
+
+$('.ok').on('click', function(){
+    $('.modal-content').animate({opacity: 0, top: -100})
+    $('.modal').hide()
+})
+
+$('.insta').on('click', function(){
+    alert("iii, Không có gì đâu :>>>")
+})
